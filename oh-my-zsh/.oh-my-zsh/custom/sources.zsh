ba9961968dcf673beb12ba9d504cc85d13a797c9
Steve Pentland <stevepentland@gmail.com>
2019-09-07 14:10:15 -0400

    Cleanup, and try to fix prompt

--- oh-my-zsh/.oh-my-zsh/custom/sources.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/sources.zsh
@@ -1,1 +1,5 @@
-source /usr/local/bin/virtualenvwrapper.sh
+if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
+	source /usr/local/bin/virtualenvwrapper.sh
+elif [ -f /usr/bin/virtualenvwrapper.sh ]; then
+	source /usr/bin/virtualenvwrapper.sh
+fi
